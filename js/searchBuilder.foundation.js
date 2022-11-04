9912242c6614f1fa6441a6a58b2d3689107f9dd2
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-04 11:46:37 +0000

    255656e3a9b77617ee18b47774da06f753491c18
    Sync to source repo @255656e3a9b77617ee18b47774da06f753491c18

--- js/searchBuilder.foundation.js
+++ js/searchBuilder.foundation.js
@@ -1,51 +1,6 @@
-
 /*! Foundation ui integration for DataTables' SearchBuilder
  * © SpryMedia Ltd - datatables.net/license
  */
-
-(function( factory ){
-	if ( typeof define === 'function' && define.amd ) {
-		// AMD
-		define( ['jquery', 'datatables.net-zf', 'datatables.net-searchbuilder'], function ( $ ) {
-			return factory( $, window, document );
-		} );
-	}
-	else if ( typeof exports === 'object' ) {
-		// CommonJS
-		module.exports = function (root, $) {
-			if ( ! root ) {
-				// CommonJS environments without a window global must pass a
-				// root. This will give an error otherwise
-				root = window;
-			}
-
-			if ( ! $ ) {
-				$ = typeof window !== 'undefined' ? // jQuery's factory checks for a global window
-					require('jquery') :
-					require('jquery')( root );
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-zf')(root, $);
-			}
-
-			if ( ! $.fn.dataTable ) {
-				require('datatables.net-searchbuilder')(root, $);
-			}
-
-
-			return factory( $, root, root.document );
-		};
-	}
-	else {
-		// Browser
-		factory( jQuery, window, document );
-	}
-}(function( $, window, document, undefined ) {
-'use strict';
-var DataTable = $.fn.dataTable;
-
-
 $.extend(true, DataTable.SearchBuilder.classes, {
     clearAll: 'button alert dtsb-clearAll'
 });
@@ -62,7 +17,3 @@ $.extend(true, DataTable.Criteria.classes, {
     right: 'button dtsb-right',
     value: 'form-control dtsb-value'
 });
-
-
-return DataTable;
-}));
