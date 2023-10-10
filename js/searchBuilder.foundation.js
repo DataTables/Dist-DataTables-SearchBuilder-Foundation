77b595f92370dd332891e640c34b302a2666db5d
Allan Jardine <allan.jardine@sprymedia.co.uk>
2023-10-10 10:01:27 +0100

    b29d74a762c29d82051e42eb4eac67ebb1794871 Fix: Styling integration for the new "Search" button feature - classes were missing
    https://datatables.net/forums/discussion/77320
    
    Sync to source repo @b29d74a762c29d82051e42eb4eac67ebb1794871

--- js/searchBuilder.foundation.js
+++ js/searchBuilder.foundation.js
@@ -66,6 +66,7 @@ $.extend(true, DataTable.Criteria.classes, {
     "delete": 'button alert dtsb-delete',
     left: 'button dtsb-left',
     right: 'button dtsb-right',
+    search: 'button dtsb-search',
     value: 'form-control dtsb-value'
 });
 
