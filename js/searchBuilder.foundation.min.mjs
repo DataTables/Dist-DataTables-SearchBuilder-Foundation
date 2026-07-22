2cf664e9e562e83a145292e24adead36241b57a5
dtbuild <dtbuild@rigil>
2026-07-22 14:50:36 +0000

    b2567bef1a733d021110553c86991410013af8de Release 2.0.0 ready
    Sync to source repo @b2567bef1a733d021110553c86991410013af8de

--- js/searchBuilder.foundation.min.mjs
+++ js/searchBuilder.foundation.min.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder Foundation styling 2.0.0-beta.1 for DataTables
+/*! SearchBuilder Foundation styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-zf";import SearchBuilder from"datatables.net-searchbuilder";Object.assign(DataTable.SearchBuilder.classes,{clearAll:"button alert dtsb-clearAll"}),Object.assign(DataTable.Group.classes,{add:"button dtsb-add",clearGroup:"button dtsb-clearGroup",logic:"button dtsb-logic",search:"button dtsb-search"}),Object.assign(DataTable.Criteria.classes,{condition:"form-control dtsb-condition",data:"form-control dtsb-data",delete:"button alert dtsb-delete",left:"button dtsb-left",right:"button dtsb-right",value:"form-control dtsb-value"});export default DataTable;
--- js/searchBuilder.foundation.mjs
+++ js/searchBuilder.foundation.mjs
@@ -1,4 +1,4 @@
-/*! SearchBuilder Foundation styling 2.0.0-beta.1 for DataTables
+/*! SearchBuilder Foundation styling 2.0.0 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 
